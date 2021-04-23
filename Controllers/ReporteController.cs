824da1a52498632e57bb2be88872dbb87a15bfc1
Diego Padilla <diegotails@hotmail.com>
2021-04-22 22:52:18 -0500

    ver1

---
+++ Controllers/ReporteController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pruebaHACSYS.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Reporte")]
+    [ApiController]
+
+    public class ReporteController : ControllerBase
+    {
+        [HttpGet]
+        public List<Reporte> Get()
+        {
+            List<Reporte> lista = null;
+
+            using (Models.vueContext db = new Models.vueContext())
+            {
+                lista = (from dato in db.Reportes
+                         select new Reporte {  
+                            Fecha = dato.Fecha,
+                            Descripcion = dato.Descripcion, 
+                            Acciones = dato.Acciones,
+                            Nombre = dato.Nombre,
+                            Estatus = dato.Estatus
+                         }
+                ).ToList();
+            }
+
+            return lista;
+        }
+    
+
+    [HttpPost]
+    public List<Models.Reporte> Post()
+    {
+            List<Models.Reporte> r = null;
+            using (Models.vueContext db = new Models.vueContext())
+        {
+            
+            r = (from dato in db.Reportes
+            select new Models.Reporte
+            {
+                IdReporte = dato.IdReporte,
+                Fecha = dato.Fecha,
+                Descripcion = dato.Descripcion,
+                Acciones = dato.Acciones,
+                Nombre = dato.Nombre,
+                Estatus = dato.Estatus
+            }).ToList();
+                foreach (Models.Reporte e in r)
+                {
+                    db.Reportes.Add(e);
+                }
+            db.SaveChanges();
+            return r;
+            };
+        }
+    }
+
+    public class Reporte
+    {
+        public string Id { get; set; }
+        public string Fecha { get; set; }
+        public string Descripcion { get; set; }
+        public string Nombre { get; set; }
+        public string Acciones { get; set; }
+        public string Estatus { get; set; }
+        public string Razon { get; set; }
+    }
+}
---
+++ Models/Reporte.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace pruebaHACSYS.Models
+{
+    public partial class Reporte
+    {
+        public string IdReporte { get; set; }
+        public string Fecha { get; set; }
+        public string Descripcion { get; set; }
+        public string Nombre { get; set; }
+        public string Estatus { get; set; }
+        public string Razon { get; set; }
+        public string Acciones { get; set; }
+    }
+}
